87c16e9d809a5905378f17400f36542f0a81a115
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-08-31 09:30:26 +1000

    Fix transcript message output

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/transcript', async ({body, query}, res, next) => {
   api.get_cdr({call_uuid: body.call_uuid}, (err, statusCode, data) => {
     if (err) return next(err)
     console.error('transcription: ', body.transcription)
-    let text = `From: ${data.from_number} at ${data.answer_time}\n${body.transcription || 'no message'}\n<https://s3.amazonaws.com/recordings_2013/${body.recording_id}.mp3|Listen>`
+    let text = `From: ${data.from_number} at ${data.answer_time}\nMessage: ${body.transcription}\n<https://s3.amazonaws.com/recordings_2013/${body.recording_id}.mp3|Listen>`
     webhook.send({text}, err => {
       if (err) return next (err)
       res.sendStatus(200);
